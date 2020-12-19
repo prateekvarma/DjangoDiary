9536fb5caffd369c7a60501a4348763492896d18
Prateek Varma <prtk.varma@gmail.com>
2020-12-18 21:09:40 -0600

    created Entry model, and display all entries on index page

--- django_diary/entries/models.py
+++ django_diary/entries/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    # this formats your DB row in admin as Entry #1, insted of Entry object (1)
+    def __str__(self):
+        return 'Entry #{}'.format(self.id)
--- django_diary/entries/views.py
+++ django_diary/entries/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from . models import Entry
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'entries/index.html')
+    # entries = Entry.objects.all()
+    # to get the latest DB entries
+    entries = Entry.objects.order_by('-date_posted')
+    return render(request, 'entries/index.html', {'entries': entries})
 
 
 def add(request):
